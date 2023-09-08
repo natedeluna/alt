7cbc24f20af804dfb0cbe6f2a7ee11b2aee4a489
natedeluna <107595860+natedeluna@users.noreply.github.com>
2023-09-07 19:23:50 -0500

    new prototypes

--- src/components/space.tsx
+++ src/components/space.tsx
@@ -3,13 +3,20 @@ import styles from './button.module.css';
 
 export const Space = component$(() => {
   return (
+    <>
     <div class= {[styles.spaceBackground]}>
       <div class = {[styles.space, styles.v1]}></div>
       <div class = {[styles.space, styles.v2]}></div>
       <div class = {[styles.space, styles.v3]}></div>
       <div class = {[styles.space, styles.v4]}></div>
       <div class = {[styles.space, styles.v5]}></div>
-      <div class = {[styles.space, styles.v6]}></div>
+      <br></br>
+      <div class={['flex', 'gap-10']}>
+        <div class = {[styles.space, styles.v6]}></div>
+        <div class = {[styles.space, styles.v7]}></div>
+        <div class = {[styles.space, styles.v8]}></div>
+      </div>
     </div>
+  </>
   );
 });
